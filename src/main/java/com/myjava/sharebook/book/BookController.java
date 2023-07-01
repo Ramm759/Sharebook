4b5ae1ad0b1cbcee9911bade5c60b73c02435c82
Ramm759 <fredgsn59@gmail.com>
2023-07-01 18:34:36 +0200

    Controller Book

---
+++ src/main/java/com/myjava/sharebook/book/BookController.java
@@ -0,0 +1,40 @@
+package com.myjava.sharebook.book;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+    @GetMapping(value = "")
+    public ResponseEntity listBooks() {
+        Book myBook = new Book();
+        myBook.setTitle("toto");
+        myBook.setCategory(new Category("Thriller"));
+        return new ResponseEntity(List.of(myBook), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity addBook(Book book){
+        // TODO : Persist
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{bookId}")
+    public ResponseEntity delBook(@PathVariable("bookId") String bookId){
+        // TODO : Persist
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(value = "/{bookId}")
+    public ResponseEntity updateBook(@PathVariable("bookId") String bookId, Book book){
+        // TODO : Persist
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+}
