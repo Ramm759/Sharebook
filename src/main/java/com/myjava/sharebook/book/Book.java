60cb0a7b062b0a7436b3bf3cd5c4dd20251929c2
Ramm759 <fredgsn59@gmail.com>
2023-07-01 23:17:24 +0200

    entité Book

--- src/main/java/com/myjava/sharebook/book/Book.java
+++ src/main/java/com/myjava/sharebook/book/Book.java
@@ -1,12 +1,50 @@
 package com.myjava.sharebook.book;
 
+import com.myjava.sharebook.user.User;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     @NotBlank
     private String title;
+
+    @ManyToOne
     private Category category;
+
     private BookStatus bookStatus;
+    @ManyToOne
+    private User user;
+
+    private BookStatus status;
+    private boolean deleted;
+
+    @Transient
+    private int categoryId;
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public BookStatus getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(BookStatus bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -20,15 +58,35 @@ public class Book {
         return category;
     }
 
-    public void setCategory(Category category) {
-        this.category = category;
+    public User getUser() {
+        return user;
     }
 
-    public BookStatus getBookStatus() {
-        return bookStatus;
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public void setBookStatus(BookStatus bookStatus) {
-        this.bookStatus = bookStatus;
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 }
