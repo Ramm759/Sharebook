68d54a42d597b6b783cac5d543c693d88335e834
Ramm759 <fredgsn59@gmail.com>
2023-07-01 20:33:42 +0200

    Book controller : listCategories

--- src/main/java/com/myjava/sharebook/book/BookController.java
+++ src/main/java/com/myjava/sharebook/book/BookController.java
@@ -35,8 +35,10 @@ public class BookController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
-
-
+    @GetMapping
+    public ResponseEntity listCategories() {
+        Category category = new Category("Thriller");
+        return new ResponseEntity(List.of(category), HttpStatus.OK);
+    }
 
 }
