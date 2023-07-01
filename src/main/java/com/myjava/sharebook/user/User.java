95756f5eb21502c584c723bc7b3ddfc0ebc1fdf4
Ramm759 <fredgsn59@gmail.com>
2023-07-01 23:20:57 +0200

    entité User

--- src/main/java/com/myjava/sharebook/user/User.java
+++ src/main/java/com/myjava/sharebook/user/User.java
@@ -1,9 +1,15 @@
 package com.myjava.sharebook.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String email;
     @Size(min = 2, max = 25, message = "Firstname Entre 2 et 25 caracteres SVP")
     private String firstName;
