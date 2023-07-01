63d940c5b91175007937d5a15a91752481f60f78
Ramm759 <fredgsn59@gmail.com>
2023-07-01 23:29:17 +0200

    entité Category

--- src/main/java/com/myjava/sharebook/book/Category.java
+++ src/main/java/com/myjava/sharebook/book/Category.java
@@ -1,8 +1,19 @@
 package com.myjava.sharebook.book;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String label;
 
+    public Category() {
+    }
     public Category(String label) {
         this.label = label;
     }
