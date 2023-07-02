0163ac08de6b002ca37de2a5393377216e3db33e
Ramm759 <fredgsn59@gmail.com>
2023-07-02 09:40:54 +0200

    CategoryRepository

--- src/main/java/com/myjava/sharebook/book/BookRepository.java
+++ src/main/java/com/myjava/sharebook/book/BookRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
     List<Book> findByStatusAndUser_IdNotAndDeletedFalse(BookStatus status, int userId);
-
     List<Book> findByUser_IdAndDeletedFalse(int userId);
 }
